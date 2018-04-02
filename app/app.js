44ab4896c75f886278f8ccbce681b25e192f0c8d
Andrey Karamnoff <karamnoff@gmail.com>
2018-04-02 23:23:13 +0300

    add 'octicons' module

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const reload = require('reload');
 const bodyParser = require('body-parser');
 const uuidv4 = require('uuid/v4');
+const octicons = require("octicons");
 
 const app = express();
 const rooms = require('./data/rooms.json');
@@ -13,6 +14,7 @@ app.set('view engine', 'pug');
 app.use(express.static('node_modules/bootstrap/dist'));
 app.use(express.static('node_modules/jquery/dist'));
 app.use(express.static('node_modules/popper.js/dist'));
+app.use(express.static('node_modules/octicons/build'));
 app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -24,7 +26,8 @@ app.get('/', (req, res) => {
 app.get('/admin/rooms', (req, res) => {
     res.render('rooms', {
         title: 'Admin Rooms',
-        rooms
+        rooms,
+        octicons
     });
 });
 
