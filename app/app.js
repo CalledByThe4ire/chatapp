324d46f3e0602a15b4c959bbb421c148ebadcb01
Andrey Karamnoff <karamnoff@gmail.com>
2018-04-03 22:45:39 +0300

    add route for rooms' deletion

--- app/app.js
+++ app/app.js
@@ -1,52 +1,58 @@
-const express = require('express');
-const reload = require('reload');
-const bodyParser = require('body-parser');
-const uuidv4 = require('uuid/v4');
+const express = require("express");
+const reload = require("reload");
+const bodyParser = require("body-parser");
+const uuidv4 = require("uuid/v4");
 const octicons = require("octicons");
 
 const app = express();
-const rooms = require('./data/rooms.json');
+let rooms = require("./data/rooms.json");
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', './app/views');
-app.set('view engine', 'pug');
+app.set("port", process.env.PORT || 3000);
+app.set("views", "./app/views");
+app.set("view engine", "pug");
 
-app.use(express.static('node_modules/bootstrap/dist'));
-app.use(express.static('node_modules/jquery/dist'));
-app.use(express.static('node_modules/popper.js/dist'));
-app.use(express.static('node_modules/octicons/build'));
-app.use(express.static('./app/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("node_modules/bootstrap/dist"));
+app.use(express.static("node_modules/jquery/dist"));
+app.use(express.static("node_modules/popper.js/dist"));
+app.use(express.static("node_modules/octicons/build"));
+app.use(express.static("./app/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
 });
 
-app.get('/admin/rooms', (req, res) => {
-    res.render('rooms', {
-        title: 'Admin Rooms',
+app.get("/admin/rooms", (req, res) => {
+    res.render("rooms", {
+        title: "Admin Rooms",
         rooms,
         octicons
     });
 });
 
-app.get('/admin/rooms/add', (req, res) => {
-    res.render('add');
+app.get("/admin/rooms/add", (req, res) => {
+    res.render("add");
 });
 
-app.post('/admin/rooms/add', (req, res) => {
+app.post("/admin/rooms/add", (req, res) => {
     const room = {
         name: req.body.name,
         id: uuidv4()
-    }
+    };
 
     rooms.push(room);
-    res.redirect('/admin/rooms/');
+    res.redirect("/admin/rooms/");
+});
+
+app.get("/admin/rooms/delete/:id", (req, res) => {
+    const roomId = req.params.id;
+    rooms = rooms.filter(room => room.id !== roomId);
+
+    res.redirect("/admin/rooms/");
 });
 
 reload(app);
 
-app.listen(app.get('port'), () => {
-    console.log(`Chat app listening on port ${app.get('port')}!`);
+app.listen(app.get("port"), () => {
+    console.log(`Chat app listening on port ${app.get("port")}!`);
 });
