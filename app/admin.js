715b69af3021bed5bd8654d16edbe08cd7a2c37f
Andrey Karamnoff <karamnoff@gmail.com>
2018-04-08 09:31:31 +0300

    pass 'baseUrl' option to admin's views

--- app/admin.js
+++ app/admin.js
@@ -6,25 +6,28 @@ const octicons = require("octicons");
 let rooms = require("./data/rooms.json");
 
 router.get("/rooms", (req, res) => {
-    res.render("rooms", { title: "Admin Rooms", rooms, octicons });
+    const baseUrl = req.baseUrl;
+    res.render("rooms", { title: "Admin Rooms", baseUrl, rooms, octicons });
 });
 
 router
     .route("/rooms/add")
     .get((req, res) => {
-        res.render("add");
+        const baseUrl = req.baseUrl;
+        res.render("add", { baseUrl });
     })
 
     .post((req, res) => {
         const room = { name: req.body.name, id: uuidv4() };
 
         rooms.push(room);
-        res.redirect(req.baseUrl + "/rooms/");
+        res.redirect(`${req.baseUrl}/rooms/`);
     });
 
 router
     .route("/rooms/edit/:id")
     .get((req, res) => {
+        const baseUrl = req.baseUrl;
         const roomId = req.params.id;
         const room = rooms.find(room => room.id === roomId);
 
@@ -33,7 +36,7 @@ router
             return;
         }
 
-        res.render("edit", { room });
+        res.render("edit", { baseUrl, room });
     })
     .post((req, res) => {
         const roomId = req.params.id;
@@ -46,14 +49,14 @@ router
 
         room.name = req.body.name;
 
-        res.redirect(req.baseUrl + "/rooms/");
+        res.redirect(`${req.baseUrl}/rooms/`);
     });
 
 router.get("/rooms/delete/:id", (req, res) => {
     const roomId = req.params.id;
     rooms = rooms.filter(room => room.id !== roomId);
 
-    res.redirect(req.baseUrl + "/rooms/");
+    res.redirect(`${req.baseUrl}/rooms/`);
 });
 
 module.exports = router;
