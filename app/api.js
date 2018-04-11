66bf212517a7af240ac09aa92898646b4debaefa
Andrey Karamnoff <karamnoff@gmail.com>
2018-04-11 22:48:34 +0300

    add route to show room's messages via 'post' method

--- app/api.js
+++ app/api.js
@@ -1,28 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const messages = require('./data/messages.json');
+const uuidv4 = require("uuid/v4");
+const messages = require("./data/messages.json");
 
-const rooms = require('./data/rooms.json');
+const rooms = require("./data/rooms.json");
 
-router.get('/rooms', (req, res) => {
+router.get("/rooms", (req, res) => {
     res.json(rooms);
 });
 
-router.get("/rooms/:roomId/messages", (req, res) => {
-    const roomId = req.params.roomId;
+router
+    .route("/rooms/:roomId/messages")
+    .get((req, res) => {
+        const roomId = req.params.roomId;
 
-    const roomMessages = messages.filter(message => message.roomId === roomId);
-    const room = rooms.find(room => room.id === roomId);
+        const roomMessages = messages.filter(
+            message => message.roomId === roomId
+        );
+        const room = rooms.find(room => room.id === roomId);
 
-    if (!room) {
-        res.sendStatus(404);
-        return;
-    }
+        if (!room) {
+            res.sendStatus(404);
+            return;
+        }
 
-    res.json({
-        room,
-        messages
+        res.json({
+            room,
+            messages
+        });
+    })
+    .post((req, res) => {
+        const roomId = req.params.roomId;
+        const message = {
+            roomId,
+            text: req.body.text,
+            userId: "44f885e8-87e9-4911-973c-4074188f408a",
+            id: uuidv4()
+        };
+        messages.push(message);
+        res.sendStatus(200);
+    })
+    .delete((req, res) => {
+        const roomId = req.params.roomId;
     });
-});
 
 module.exports = router;
