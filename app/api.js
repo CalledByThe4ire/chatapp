98f240b83a408b62a80539df43fe465054e55fa5
Andrey Karamnoff <karamnoff@gmail.com>
2018-04-11 22:32:07 +0300

    add route to show room's messages via 'get' method

--- app/api.js
+++ app/api.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const messages = require('./data/messages.json');
 
 const rooms = require('./data/rooms.json');
 
 router.get('/rooms', (req, res) => {
     res.json(rooms);
+});
+
+router.get("/rooms/:roomId/messages", (req, res) => {
+    const roomId = req.params.roomId;
+
+    const roomMessages = messages.filter(message => message.roomId === roomId);
+    const room = rooms.find(room => room.id === roomId);
+
+    if (!room) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.json({
+        room,
+        messages
+    });
 });
 
 module.exports = router;
